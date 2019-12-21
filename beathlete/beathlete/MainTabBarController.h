48804bc95f7f949e9f0583d2e6b7ff1db2f24102
Konrad Wilczek <wolframowsky@gmail.com>
2019-12-21 19:05:36 +0100

    Added constraints to table views

---
+++ beathlete/beathlete/MainTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.h
+//  beathlete
+//
+//  Created by Admin on 21/12/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ beathlete/beathlete/MainTabBarController.m
@@ -0,0 +1,33 @@
+//
+//  MainTabBarController.m
+//  beathlete
+//
+//  Created by Admin on 21/12/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import "MainTabBarController.h"
+
+@interface MainTabBarController ()
+
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
